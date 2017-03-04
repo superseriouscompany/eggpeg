50aee7b3165987b6a4bdef2ddb6ca6105abaeeae
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-04 01:06:59 +0000

    Integrate branch for android

--- android/app/src/main/java/com/sniper/MainActivity.java
+++ android/app/src/main/java/com/sniper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sniper;
 
 import com.facebook.react.ReactActivity;
+import io.branch.rnbranch.*;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,16 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "sniper";
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RNBranchModule.initSession(this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        this.setIntent(intent);
     }
 }
--- android/app/src/main/java/com/sniper/MainApplication.java
+++ android/app/src/main/java/com/sniper/MainApplication.java
@@ -12,6 +12,10 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+// import Branch and RNBranch
+import io.branch.rnbranch.*;
+import io.branch.referral.Branch;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -37,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+      Branch.getAutoInstance(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
