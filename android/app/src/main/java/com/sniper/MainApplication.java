1f05dc479da138226567ee2575bb68a6f4d830b3
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-05 16:21:06 +0000

    Set timeouts to 0 when running on emulator

--- android/app/src/main/java/com/sniper/MainApplication.java
+++ android/app/src/main/java/com/sniper/MainApplication.java
@@ -3,6 +3,7 @@ package com.sniper;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.idehub.Billing.InAppBillingBridgePackage;
 import io.branch.rnbranch.RNBranchPackage;
 import com.facebook.react.ReactNativeHost;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new InAppBillingBridgePackage(),
             new RNBranchPackage()
       );
--- app/config.js
+++ app/config.js
@@ -1,7 +1,9 @@
+const DeviceInfo = require('react-native-device-info')
+
 const config = {
-  winDelay: 0,
+  winDelay: 1000,
   bullet: {
-    delay:  0,
+    delay:  3000,
     linger: 100,
   },
   chamber: 3,
@@ -14,6 +16,12 @@ const config = {
     max: 100,
     penalty: 2,
   },
+}
+
+
+if( DeviceInfo.isEmulator() ) {
+  config.winDelay     = 0;
+  config.bullet.delay = 0;
 }
 
 export default config
