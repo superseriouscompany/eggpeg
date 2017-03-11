61ffd31dd8707560e80177d7b55f1662b0c86059
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-11 23:06:16 +0000

    Give an egg back when hitting a target

--- app/reducers/chamber.js
+++ app/reducers/chamber.js
@@ -4,6 +4,8 @@ export default function(state = config.chamber, action) {
   switch(action.type) {
     case 'bullets:fire':
       return Math.max(0, state - 1);
+    case 'bullets:hit':
+      return state + 1
     case 'level:clear':
       return config.chamber;
     default:
