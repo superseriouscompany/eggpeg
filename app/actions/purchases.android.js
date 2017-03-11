7a0fe37be9dcca6b1671204d5f3f0211381472ec
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-11 20:03:05 +0000

    Do InAppBilling blind

--- app/actions/purchases.android.js
+++ app/actions/purchases.android.js
@@ -1,13 +1,32 @@
+import InAppBilling from 'react-native-billing'
+
 export function loadProducts() {
   return function(dispatch) {
-    const products = [{
-      priceString: '$0.99'
-    }]
-
-    dispatch({type: 'purchase:loadProducts', products: products})
+    InAppBilling.getProductDetails('com.superserious.eggpeg.continue').then((p) => {
+      dispatch({type: 'purchase:loadProducts', products: [{
+        title: p.title,
+        description: p.description,
+        priceString: p.priceText,
+        currency: p.currency,
+      }]})
+    }).catch((err) => {
+      alert(err.message || JSON.stringify(err))
+    });
   }
 }
 
-export function purchase(identifier) {
-  return alert('Not implemented on android')
+export function purchase(identifier, cb) {
+  return function(dispatch) {
+    InAppBilling.open().then(() => {
+      return InAppBilling.purchase(identifier);
+    }).then((details) => {
+      purchaseToken = details.purchaseToken;
+      return InAppBilling.consumePurchase(identifier);
+    }).then(() => {
+      cb(null, true)
+      InAppBilling.close()
+    }).catch((err) => {
+      cb(err);
+    })
+  }
 }
