4777b8b731afb144b4382896a3bde6250d89c503
Santiago Garza <sgarza62@yahoo.com>
2017-03-12 00:12:29 +0000

    Animate the rainbow

--- app/components/RainbowBar.js
+++ app/components/RainbowBar.js
@@ -5,28 +5,47 @@ import Component from './Component';
 import base from '../styles/base'
 import {
   Animated,
+  Dimensions,
+  Easing,
   StyleSheet,
   View,
 } from 'react-native';
 
+const screenWidth = Dimensions.get('window').width;
+
 export default class RainbowBar extends Component {
-  render() {
-    return(
-      <View style={style.barContainer}>
-        <Bar color={base.colors.green} />
-        <Bar color={base.colors.yellow} />
-        <Bar color={base.colors.orange} />
-        <Bar color={base.colors.red} />
-        <Bar color={base.colors.purple} />
-        <Bar color={base.colors.blue} />
-      </View>
-    )
-  }
+  constructor(props) {
+     super(props);
+     this.state = {
+       offsetX: new Animated.Value(-screenWidth * 2),
+     };
+   }
+
+   componentDidMount() {
+     Animated.timing(
+       this.state.offsetX,
+       {toValue: screenWidth / 2, duration: 1000, delay: 150}
+     ).start();
+   }
+
+   render() {
+     return (
+       <Animated.View
+       style={[this.props.style, style.barContainer, {transform: [{translateX: this.state.offsetX}]}]}>
+         <Bar style={{width: screenWidth * 2}} color={base.colors.green} />
+         <Bar style={{width: screenWidth * 1.8}} color={base.colors.yellow} />
+         <Bar style={{width: screenWidth * 1.6}} color={base.colors.orange} />
+         <Bar style={{width: screenWidth * 1.4}} color={base.colors.red} />
+         <Bar style={{width: screenWidth * 1.2}} color={base.colors.purple} />
+         <Bar style={{width: screenWidth * 1}} color={base.colors.blue} />
+       </Animated.View>
+     );
+   }
 }
 
 function Bar(props) {
   return (
-    <View style={[style.bar, {backgroundColor: props.color}]} />
+    <View style={[props.style, style.bar, {backgroundColor: props.color}]} />
   )
 }
 
@@ -35,9 +54,9 @@ const barHeight = 25;
 const style = StyleSheet.create({
   bar: {
     height: barHeight,
-    width: '100%'
   },
   barContainer: {
     position: 'absolute',
+    width: screenWidth * 2,
   },
 });
