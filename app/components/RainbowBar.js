884ad698146d7c4b86dc0b30b761f0b68eb6763d
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-19 21:00:54 +0000

    Clean up RainbowBar formatting

--- app/components/RainbowBar.js
+++ app/components/RainbowBar.js
@@ -12,9 +12,16 @@ import {
   View,
 } from 'react-native';
 
-const screenWidth = Dimensions.get('window').width;
+const screenWidth      = Dimensions.get('window').width;
+const defaultBarHeight = 25;
 
 export default class RainbowBar extends Component {
+  static propTypes = {
+    finalOffset: PropTypes.number,
+    leave:       PropTypes.bool,
+    complete:    PropTypes.func,
+  }
+
   constructor(props) {
      super(props);
      this.state = {
@@ -22,10 +29,9 @@ export default class RainbowBar extends Component {
      };
    }
 
-   // TODO: make a more coherent API for this component
    componentDidMount() {
      Animated.timing(this.state.offsetX, {
-       toValue: this.props.finalOffset + 10 || (screenWidth / 2),
+       toValue: this.props.finalOffset ? this.props.finalOffset + 10 : (screenWidth / 2),
        duration: config.timings.rainbow,
        delay: config.timings.rainbowDelay
      }).start(() => {
@@ -48,16 +54,15 @@ export default class RainbowBar extends Component {
 
    render() {
      return (
-       <Animated.View
-       style={[this.props.style, style.barContainer, {
+       <Animated.View style={[this.props.style, style.barContainer, {
          transform: [{translateX: this.state.offsetX}]
        }]}>
-         <Bar style={{width: screenWidth * 2, height: this.props.barHeight || barHeight }}    color={base.colors.green} />
-         <Bar style={{width: screenWidth * 1.8, height: this.props.barHeight || barHeight }} color={base.colors.yellow} />
-         <Bar style={{width: screenWidth * 1.6, height: this.props.barHeight || barHeight}}  color={base.colors.orange} />
-         <Bar style={{width: screenWidth * 1.4, height: this.props.barHeight || barHeight}}  color={base.colors.red} />
-         <Bar style={{width: screenWidth * 1.2, height: this.props.barHeight || barHeight}}  color={base.colors.purple} />
-         <Bar style={{width: screenWidth * 1, height: this.props.barHeight || barHeight}}    color={base.colors.blue} />
+         <Bar color={base.colors.green}  style={{width: screenWidth * 2,   height: this.props.barHeight || defaultBarHeight}} />
+         <Bar color={base.colors.yellow} style={{width: screenWidth * 1.8, height: this.props.barHeight || defaultBarHeight}} />
+         <Bar color={base.colors.orange} style={{width: screenWidth * 1.6, height: this.props.barHeight || defaultBarHeight}} />
+         <Bar color={base.colors.red}    style={{width: screenWidth * 1.4, height: this.props.barHeight || defaultBarHeight}} />
+         <Bar color={base.colors.purple} style={{width: screenWidth * 1.2, height: this.props.barHeight || defaultBarHeight}} />
+         <Bar color={base.colors.blue}   style={{width: screenWidth * 1,   height: this.props.barHeight || defaultBarHeight}} />
        </Animated.View>
      );
    }
@@ -69,12 +74,7 @@ function Bar(props) {
   )
 }
 
-const barHeight = 25;
-
 const style = StyleSheet.create({
-  bar: {
-    height: barHeight,
-  },
   barContainer: {
     position: 'absolute',
     width: screenWidth * 2,
