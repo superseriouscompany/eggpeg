fac8e3ecc60977cdda8b49d875f0cd9b07fd665b
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-22 15:32:21 +0000

    Checkpoint: don't rehydrate scene if it was game over

--- app/reducers/scene.js
+++ app/reducers/scene.js
@@ -1,4 +1,11 @@
-export default function(state = {current: 'Start', previous: null}, action) {
+import {REHYDRATE} from 'redux-persist/constants'
+
+const initialState = {
+  current: 'Start',
+  previous: null,
+}
+
+export default function(state = initialState, action) {
   switch(action.type) {
     case 'scene:change':
       return {
@@ -10,6 +17,15 @@ export default function(state = {current: 'Start', previous: null}, action) {
         current: state.previous || 'Start',
         previous: null,
       }
+    case REHYDRATE:
+      console.warn('rehydrating')
+      const incoming = action.payload.scene;
+      if( !incoming ) { return {...initialState} }
+      const {level}  = action.payload;
+      if( incoming.current == 'Game' && level.done && !level.win ) {
+        return {...initialState}
+      }
+      return {...initialState, ...incoming}
     default:
       return state
   }
