2a2dc3dcd50d5b95c5ca2a3bf33602ef9f92ca9e
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-03-31 18:16:52 +0100

    Extremely annoying fade in/fade out animation

--- app/components/Stage.js
+++ app/components/Stage.js
@@ -9,35 +9,85 @@ import World              from './World';
 import Worlds             from './Worlds'
 import HallOfFame         from './HallOfFame'
 import {
-  View
+  Animated,
+  StyleSheet,
+  View,
 } from 'react-native'
 
 class Stage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      enterAnim: new Animated.Value(1),
+      scene:     props.scene,
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    if( props.scene != this.props.scene ) {
+      this.state.enterAnim.setValue(0)
+      this.setState({
+        nextScene: props.scene,
+      })
+      Animated.timing(this.state.enterAnim, {
+        duration: 1000, toValue: 1,
+      }).start(() => {
+        this.setState({
+          scene: props.scene,
+          nextScene: null,
+        })
+      })
+    }
+  }
+
   render() { return (
-    <View style={{flex: 1}}>
-      { this.props.scene == 'World' ?
-        <World />
-      : this.props.scene == 'AboutUs' ?
-        <FollowUs />
-      : this.props.scene == 'Start' ?
-        <Start />
-      : this.props.scene == 'Worlds' ?
-        <Worlds {...this.props.sceneProps} />
-      : this.props.scene == 'Settings' ?
-        <Settings />
-      : this.props.scene == 'HallOfFame' ?
-        <HallOfFame {...this.props.sceneProps}/>
-      :
-        <View style={{backgroundColor: 'indianred', width: 100, height: 100}}/>
-      }
+    <View style={style.container}>
+      <View style={style.container}>
+        { this.showScene(this.state.scene, this.state.sceneProps)}
+      </View>
+
+      { this.state.nextScene ?
+        <View style={style.overlay}>
+          <Animated.View style={[style.container, {
+            opacity: this.state.enterAnim.interpolate({
+              inputRange:  [0, 1],
+              outputRange: [0, 1],
+            })
+          }]}>
+            {
+              this.showScene(this.state.nextScene, this.state.nextSceneProps)
+            }
+          </Animated.View>
+        </View>
+      : null }
     </View>
   )}
+
+  showScene(scene, sceneProps) {
+    switch(scene) {
+      case 'World':
+        return <World />
+      case 'AboutUs':
+        return <FollowUs />
+      case 'Start':
+        return <Start />
+      case 'Worlds':
+        return <Worlds {...sceneProps} />
+      case 'Settings':
+        return <Settings />
+      case 'HallOfFame':
+        return <HallOfFame {...sceneProps} />
+      default:
+        return <View style={{backgroundColor: 'indianred', width: 100, height: 100}}/>
+    }
+  }
 }
 
 function mapStateToProps(state) {
   return {
     scene:      state.scene.current,
     sceneProps: state.scene.props,
+    animation:  state.scene.animation,
   }
 }
 
@@ -46,5 +96,18 @@ function mapDispatchToProps(dispatch) {
 
   }
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  overlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Stage);
